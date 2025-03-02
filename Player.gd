cabd9753cbd684873aed59e990c805e7abf81553
IsaacS2Hud <sanchezisaac809@gmail.com>
2025-03-01 19:19:35 -0600

    Player sprite and velocity flip when camera flips
    The x-velocity and sprite of the player character flip in their process functions when the "Flip" input is pressed, just like with the CameraManager. With extra time, these node scripts should be edited to rely on a function call from the CameraManager, but first the cameras must follow the player's horizontal movement.

--- Player.gd
+++ Player.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var direction : int = 1
+
+func _process(delta: float) -> void:
+	if (Input.is_action_just_pressed("Flip")):
+		direction *= -1
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,7 +21,6 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = 1
 	velocity.x = direction * SPEED
 	
 	move_and_slide()
