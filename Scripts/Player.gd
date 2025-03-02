89aa79d50cae04fa8c4a771860827ce1324f16d7
IsaacS2Hud <sanchezisaac809@gmail.com>
2025-03-01 19:50:58 -0600

    Adds platforms and gravity to Level scene
    Player character's gravity is reapplied, with temporary physics-layered tile platforms serving as level design. Now it's time for a time for a short pause to give the player a chance to reorient themselves.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -12,8 +12,7 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		pass
-		#velocity += get_gravity() * delta
+		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
