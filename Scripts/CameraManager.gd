4135b01b4a8a62ff336bbc973432a54e1a6138bc
IsaacS2Hud <sanchezisaac809@gmail.com>
2025-03-01 19:39:45 -0600

    Cameras follow player character's position
    The cameras follow the player character's position in the Level scene, always providing the same camera offset so the player can see more towards the right of where they're facing. Now platforms and gravity must be applied to test platforming.

--- Scripts/CameraManager.gd
+++ Scripts/CameraManager.gd
@@ -1,12 +1,16 @@
-extends Node
+extends Node2D
 var activeCamera: Camera2D
 @onready var front: Camera2D = $Front
 @onready var back: Camera2D = $Back
+@onready var player: CharacterBody2D = %Player
 
 func _ready() -> void:
 	activeCamera = front
+	print("check")
 
 func _process(delta: float) -> void:
+	position.x = player.position.x
+	
 	if (Input.is_action_just_pressed("Flip")):
 		print("flipping camera")
 		activeCamera.enabled = false
