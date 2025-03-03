7794180d8674aa6cb8355e37d749f4c3e5008c05
IsaacS2Hud <sanchezisaac809@gmail.com>
2025-03-03 10:14:35 -0600

    Adds coyote physics
    The player character's groundBuffer variable stores the time since the player was on-ground, and is used to allow the player to jump even when off-ground for a brief instance. Now, it's time to implement checkpoints.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -14,10 +14,14 @@ var frozen : bool = false
 var direction : int = 1
 var jumpBuffer : float
 var jumpBufferTime : float = 0.09
+var groundBuffer : float
+var groundBufferTime : float = 0.09
 
 func _process(delta: float) -> void:
 	if (jumpBuffer > 0):
 		jumpBuffer -= delta
+	if (groundBuffer > 0):
+		groundBuffer -= delta
 
 
 func _physics_process(delta: float) -> void:
@@ -26,12 +30,15 @@ func _physics_process(delta: float) -> void:
 		if not is_on_floor():
 			velocity += get_gravity() * delta
 		
-		elif (jumpBuffer > 0):
-			_jump()
+		else:
+			if (jumpBuffer > 0):
+				_jump()
+			else:
+				groundBuffer = groundBufferTime
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept"):
-		if (is_on_floor() && !frozen):
+		if ((is_on_floor() || groundBuffer > 0) && !frozen):
 			_jump()
 		else:
 			jumpBuffer = jumpBufferTime
@@ -60,6 +67,7 @@ func _return_velocity():
 
 func _jump():
 	velocity.y = JUMP_VELOCITY
+	groundBuffer = 0
 	jumpBuffer = 0
 
 
