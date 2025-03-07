a803da3e379697ef081b239b855b917c3cceb63b
IsaacS2Hud <sanchezisaac809@gmail.com>
2025-03-07 16:04:19 -0600

    Adds new sprites and tiles
    Spike tiles replace the previous glob sprites, with spite hitboxes underneath each group of spike tiles. The enemy sprite has also been replaced, and checkpoint sprites are added and change whenever they're touched by the player character.

--- Scripts/Checkpoint.gd
+++ Scripts/Checkpoint.gd
@@ -3,6 +3,7 @@ class_name Checkpoint
 extends Area2D
 
 var checked : bool = false
+@onready var checkpoint_sprite: Sprite2D = $CheckpointSprite
 
 
 func _on_body_entered(body: Node2D) -> void:
@@ -10,7 +11,9 @@ func _on_body_entered(body: Node2D) -> void:
 		print("checkpoint!")
 		checked = true
 		body._sent_spawn_point(position)
+		checkpoint_sprite.frame_coords.x += 1
 
 
 func _reset_checkpoint():
 	checked = false
+	if (checkpoint_sprite.frame_coords.x > 0): checkpoint_sprite.frame_coords.x = 0
